5d97b19b35f7c3311405817c7255118dea8bdf3e
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-07-13 09:29:29 +0530

    Add files via upload

---
+++ BruteForce.java
@@ -0,0 +1,30 @@
+import java.util.*;;
+class BruteForce
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Text");
+        String t=s.nextLine();
+        System.out.println("Enter Pattern");
+        String p=s.nextLine();
+        int n=t.length();
+        int m=p.length();
+        for(int i=0;i<=n-m;i++)
+        {
+            int j;
+            for(j=0;j<m;j++)
+            {
+                if(t.charAt(i+j)==p.charAt(j)){}
+                else
+                    break;
+            }
+            if(j==m)
+            {
+                System.out.println("Pattern Matched");
+                return;
+            }
+        }
+        System.out.println("Pattern not matched");
+    }
+}
